33743dbecb781001318aebdd8fd7a015e77898d6
Chris Sullivan <cr_sullivan@hotmail.com>
2019-09-02 20:21:16 +1200

    Factor out into classes

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,25 @@ class GHFlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GHFlutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('GHFlutter'),
-        ),
-        body: Center(
-          child: Text('GHFlutter'),
-        ),
-      ),
+      title: 'Catholic Mass Auckland',
+      home: GHFlutter(),
     );
   }
+}
+
+class GHFlutterState extends State<GHFlutter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        title: Text('Catholic Mass Auckland'),
+      ),
+      body: Text('God Bless'),
+    );
+  }
+}
+
+class GHFlutter extends StatefulWidget {
+  @override
+  createState() => GHFlutterState();
 }
